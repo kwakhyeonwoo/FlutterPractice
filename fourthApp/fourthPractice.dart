ea593f2e40b556101a16cb38bcc7f9cadce2b6ff
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-05-22 10:23:58 +0900

    Update fourthPractice.dart

--- fourthApp/fourthPractice.dart
+++ fourthApp/fourthPractice.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime firstDay = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +19,9 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _DDay(),
+            _DDay(
+              onHeartPressed: onHeartPressed,
+            ),
             _CoupleImage(),
           ],
         ),
@@ -24,46 +30,63 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+void onHeartPressed(){//onHeartPressed 클릭시 발생하는 이벤트
+  print('클릭');
+}
+
 class _DDay extends StatelessWidget {
+    final GestureTapCallback onHeartPressed;//onHeartPressed 탭이 발생했을때 이벤트
+
+  _DDay({
+    required this.onHeartPressed,// onHeartPressed 클릭시 재 요청 되는 함수
+});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {//_DDay 관련 UI 입력
+    final textTheme = Theme.of(context).textTheme;
     return Column(
       children: [
         const SizedBox(height: 16.0),
         Text(
           'U&I',
+          style: textTheme.headline1,
         ),
         const SizedBox(height: 16.0),
         Text(
           '우리 처음 만난 날',
+          style: textTheme.bodyText1,
         ),
         Text(
           '????.??.??',
+          style: textTheme.bodyText2,
         ),
         const SizedBox(height: 16.0),
         IconButton(
-            onPressed: (){},
+            onPressed: onHeartPressed,
             icon: Icon(
               Icons.favorite,
+              color: Colors.red,
             ),
         ),
         const SizedBox(height: 16.0),
         Text(
           'D+@',
+          style: textTheme.headline2,
         ),
       ],
     );
   }
 }
 
-class _CoupleImage extends StatelessWidget {
+class _CoupleImage extends StatelessWidget {//_CoupleImage UI 작성
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(
-        fit: BoxFit.fitHeight,
-        'asset/image_2.jpg',
-        height: MediaQuery.of(context).size.height/2,
+    return Expanded(
+      child: Center(
+        child: Image.asset(
+          fit: BoxFit.fitHeight,
+          'asset/image_2.jpg',
+          height: MediaQuery.of(context).size.height,
+        ),
       ),
     );
   }
